dfabf984d09ecdf48525092fd10673825519b343
Agata Rojewska <agata.rojewska@gmail.com>
2023-04-01 10:13:43 +0200

    Finish bitcoin's website

--- Bitcoin/js/main.js
+++ Bitcoin/js/main.js
@@ -1,20 +1,25 @@
-const navMobile = document.querySelector('.nav-mobile');
-const navBtn = document.querySelector('.hamburger');
-const footerYear = document.querySelector('.footer__year')
+const navMobile = document.querySelector(".nav-mobile")
+const navBtn = document.querySelector(".hamburger")
+const allNavItems = document.querySelectorAll('.nav__link')
+const footerYear = document.querySelector(".footer__year")
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active');
+  navBtn.classList.toggle("is-active")
+  navMobile.classList.toggle("nav-mobile--active")
 
+      allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navBtn.classList.remove("is-active")
+            navMobile.classList.remove("nav-mobile--active")
+        })
+      })
 }
 
-
-
-navBtn.addEventListener('click', handleNav)
+navBtn.addEventListener("click", handleNav)
 
 const handleCurrentYear = () => {
-    const year = new Date().getFullYear()
-    footerYear.innerText = year
-  }
+  const year = new Date().getFullYear()
+  footerYear.innerText = year
+}
 
-  handleCurrentYear();
+handleCurrentYear()
