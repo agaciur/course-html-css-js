e0de0e2dbdc55eed88e25bc47e04c0ef2aa53b84
Agata Rojewska <agata.rojewska@gmail.com>
2023-04-08 13:32:21 +0200

    Quest JS

---
+++ js-zadania/js/main.js
@@ -0,0 +1,38 @@
+// const name = 'Agata';
+// const namePet = 'Kicia';
+
+// console.log(`Cześć, jestem ${name}, a to jest ${namePet} - mój kot.`);
+
+// const agePet = 4
+
+// console.log(namePet + ' ma już ' + agePet + ' lata!');
+
+// const firstName = 'Tomek'
+// let age = 12
+// age = 18
+ 
+// const favColor = 'niebieski'
+// const favMeal = 'schabowy'
+ 
+// let currentCar
+// currentCar = 'Audi'
+ 
+// let secondName = 'Ania'
+// let age2 = 24
+// let favColor2 = 'czerwony'
+
+const text1 = 'powiększ mnie!'
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+const text4 = 'sprawdź, czy zawieram słowo "czy"'
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+const text7 = 'podziel, ten, string, od, przecinków'
+
+console.log(text1.toUpperCase());
+console.log(text2.toLowerCase());
+console.log(text3.substring(6))
+console.log(text4.includes("czy"))
+console.log(text5.charAt(2))
+console.log(text6.replaceAll('pies','kot'))
+console.log(text7.split(","));
